6660d9cb707f58c7a23dbf1dd9df7b1075328836
mrowaha <ashfaqrowaha@gmail.com>
2025-05-13 08:46:34 +0300

    add unittests for login page

--- test/selenium/login.py
+++ test/selenium/login.py
@@ -1,13 +1,8 @@
-import time
 import unittest
-import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
 
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
@@ -71,6 +66,21 @@ class LoginTest(unittest.TestCase):
         self.assertIsNotNone(password_error_toast,
                              "Password Erorr Toast should be visible")
 
+    def test_invalid_email_format(self: "LoginTest"):
+        self.email_input_field.send_keys("not-an-email")
+        self.sign_in_button.click()
+        email_error_toast = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.ID, "login-form__error:email"))
+        )
+        self.assertIsNotNone(
+            email_error_toast, "Email Error Toast should be visible")
+
+        expected_message = "Invalid email address"
+        actual_message = email_error_toast.text.strip()
+        self.assertEqual(actual_message, expected_message,
+                         f"Expected toast message '{expected_message}', got '{actual_message}'")
+
     def test_unauthorized_login(self: "LoginTest"):
         self.email_input_field.send_keys("john.doe@example.com")
         self.password_input_field.send_keys("b")
@@ -82,31 +92,45 @@ class LoginTest(unittest.TestCase):
         self.assertIsNotNone(unauth_error_toast,
                              "Unauthorized should be visible")
 
-        # or "No such user found with that email."
         expected_message = "Incorrect password. Please try again."
         actual_message = unauth_error_toast.text.strip()
         self.assertEqual(actual_message, expected_message,
                          f"Expected toast message '{expected_message}', got '{actual_message}'")
 
-    # def test_successful_login(self: "LoginTest"):
-    #     self.email_input_field.send_keys("john.doe@example.com")
-    #     self.password_input_field.send_keys("P@ssw0rd123")
-    #     self.sign_in_button.click()
+    def test_no_such_user(self: "LoginTest"):
+        self.email_input_field.send_keys("a@b.com")
+        self.password_input_field.send_keys("b")
+        self.sign_in_button.click()
+        unauth_error_toast = WebDriverWait(self.driver, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.ID, "login-form__error:unauthorized"))
+        )
+        self.assertIsNotNone(unauth_error_toast,
+                             "Unauthorized should be visible")
 
-    #     expected_url = "http://localhost:3000/dashboard"
-    #     WebDriverWait(self.driver, 10).until(
-    #         lambda d: d.current_url == expected_url)
-    #     self.assertEqual(self.driver.current_url, expected_url,
-    #                      "Expected Url after successful login should be /dashboard")
+        expected_message = "No such user found with that email."
+        actual_message = unauth_error_toast.text.strip()
+        self.assertEqual(actual_message, expected_message,
+                         f"Expected toast message '{expected_message}', got '{actual_message}'")
 
-    # def test_access_denied(self: "LoginTest"):
-    #     self.driver.get("http://localhost:3000/dashboard")
-    #     denied_message = WebDriverWait(self.driver, 10).until(
-    #         expected_conditions.presence_of_element_located(
-    #             (By.ID, "dashboard__access-denied"))
-    #     )
-    #     self.assertIsNotNone(denied_message,
-    #                          "User should be not authorized")
+    def test_successful_login(self: "LoginTest"):
+        self.email_input_field.send_keys("john.doe@example.com")
+        self.password_input_field.send_keys("P@ssw0rd123")
+        self.sign_in_button.click()
+
+        expected_url = "http://localhost:5173/dashboard"
+        WebDriverWait(self.driver, 10).until(
+            lambda d: d.current_url == expected_url)
+        self.assertEqual(self.driver.current_url, expected_url,
+                         "Expected Url after successful login should be /dashboard")
+
+    def test_access_denied(self: "LoginTest"):
+        self.driver.get("http://localhost:5173/dashboard")
+        expected_url = "http://localhost:5173/"
+        WebDriverWait(self.driver, 5).until(
+            lambda d: d.current_url == expected_url)
+        self.assertEqual(self.driver.current_url, expected_url,
+                         "User should be not authorized and redirected to login")
 
 
 if __name__ == '__main__':
